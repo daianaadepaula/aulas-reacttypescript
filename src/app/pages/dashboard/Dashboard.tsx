d987bc8b91df16fc09080a326a16c7f8d45ab7ff
Daiana de Paula <daianaadepaula1@gmail.com>
2022-10-02 08:46:35 -0300

    feat: add useRef (counter)

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,11 +1,18 @@
+import { useRef } from "react"
 import { Link } from "react-router-dom"
 
 
 export const Dashboard = () => {
+	const conunterRef = useRef(0)
 
 	return (
 		<div>
 			<p>Dashboard</p>
+
+			<p>Contador: {conunterRef.current}</p>
+
+			<button onClick={() => conunterRef.current++}>Somar</button>
+			<button onClick={() => console.log(conunterRef.current++)}>Log</button>
 
 			<Link to="/entrar">Login</Link>
 		</div>
