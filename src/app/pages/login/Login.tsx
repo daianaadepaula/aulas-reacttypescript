9625c635b59ceca4c3df0676141c523d255168ca
Daiana de Paula <daianaadepaula1@gmail.com>
2022-10-19 14:58:12 -0300

    feat: childreen components

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useMemo, useRef, useState } from "react"
 
-import { InputLogin } from "./components/inputLogin"
+import { InputLogin } from "./components/InputLogin"
+import { ButtonLogin } from "./components/ButtonLogin"
 
 export const Login = () => {
 	const inputPasswordRef = useRef<HTMLInputElement>(null)
@@ -39,7 +40,18 @@ export const Login = () => {
 					onChange={newValue => setPassword(newValue)}
 				/>
 
-				<button type="button" onClick={handleEntrar}>Entrar</button>
+				{/* <button type="button" onClick={handleEntrar}>
+					Entrar
+				</button> */}
+
+				<ButtonLogin type="button" onClick={handleEntrar}>
+					Entrar
+				</ButtonLogin>
+
+				<ButtonLogin type="button" onClick={handleEntrar}>
+					Cadastrar-se
+				</ButtonLogin>
+
 			</form>
 		</div>
 	)
